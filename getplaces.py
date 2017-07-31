27bddd7cbf7282db88b14df0fe996f69c813d7b3
ducvu2212 <ducvu2212@users.noreply.github.com>
2017-07-31 11:36:37 +0700

    Update getplaces.py

--- getplaces.py
+++ getplaces.py
@@ -1,18 +1,19 @@
-import requests
 import os
 import json
-
+import requests
 
 key = os.getenv('key')
 str = raw_input('Enter your location: \n')
+#payload = {'str': str, 'key': key}
 url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + str + key
-
 
 def getplaces() :
 	"This function returns the location of a given place" 
 	r = requests.get(url)
-	output_dict = json.loads(r)
-	#output_dict.get('location')
-	return output_dict['location']
+	j = json.loads(r.text)
+	lat = j['latitude']
+	lon = j['longitude']
+	print ('latitude is: \n', lat)
+	print ('longtitude is: \n', long)
 
 print getplaces()		
