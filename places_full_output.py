db83940479546e94c6c25895884070ac5b644a7d
ducvu2212 <ducvu2212@users.noreply.github.com>
2017-08-01 20:43:34 +0700

    Delete places_full_output.py

--- places_full_output.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-r = requests.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=20.99527,105.855783&radius=2000&type=electronics_store&keyword=si%C3%AAu+thi%CC%A3+%C4%91i%C3%AA%CC%A3n+ma%CC%81y&key=AIzaSyDXmq7S5L3w9otA1HmYziQ77f8oTgltzp0')
-#r.json()
-print r.text
