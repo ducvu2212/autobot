39979ca479235ddedefcb85a81aef2c9504ba81c
ducvu2212 <ducvu2212@users.noreply.github.com>
2017-08-01 20:57:56 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,23 +3,17 @@ import json
 import requests
 
 key = os.getenv('key')
-
-def url1(): 
-	"This function returns GG Search API URL"
-	
-	str = raw_input('Enter your location: \n')
-	payload = {
+str = raw_input('Enter your location: \n')
+payload = {
 		'query': str, 
 		'key': key
-	}
-	url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?'
+}
+url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?'
 	
 	
 	
 def getlocation():
 	"This function returns the location of a given place" 
-	a = url1()
-	url = a['url']
 	r = requests.get(url, params=payload)
 	j = json.loads(r.text)
 	
@@ -30,24 +24,23 @@ def getlocation():
 			lng = item['geometry']['location']['lng']
 	else: 
 		print j['status']
+	return lat, lng
 
-def url2(): 
-	location = getlocation()
-	lat = location['lat']
-	lng = location['lng']
-	payload2 = {
+location = getlocation()
+lat = location.lat
+lng = location.lng
+payload2 = {
 		'location': [lat, lng], 
 		'radius': 4000, 
 		'type': 'electronics_store', 
 		'keyword': 'sieu+thi+dien+may', 
 		'key': key
-	}
+}
 
-	url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
+url2 = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
 
 def getaddress():
 	"This function returns nearest addresses of a given location"
-	url = url2()
 	r2 = requests.get(url2, params=payload2)
 	return r2.url
 	
