5b82cc2045253002c2a2e95ffa6e221991a85c24
ducvu2212 <ducvu2212@users.noreply.github.com>
2017-08-03 18:18:25 +0700

    Update getaddress.py

--- getaddress.py
+++ getaddress.py
@@ -1,60 +1,68 @@
-import os
-import sys
+#!/usr/bin/python
+import os, sys
 import json
 import requests
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 key = os.getenv('key')
+type = os.getenv('type')
+keyword = os.getenv('keyword')
+
 str = raw_input('Enter your location: \n')
-payload = {
-		'query': str, 
-		'key': key
-}
-url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?'
-	
-	
+
+def geturl1():
+	"This function returns the URL for location search via API"
+	payload = {
+			'query': str, 
+			'key': key
+	}
+	url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?'
+	return url, payload
 	
 def getlocation():
 	"This function returns the location of a given place" 
-	r = requests.get(url, params=payload)
-	j = json.loads(r.text)
+	u = geturl1()
+	r = requests.get(u[0], params=u[1])
+	output = json.loads(r.text)
+
+	if output['status'] == 'OK':
 	
-	if j['status'] == 'OK':
-	
-		for item in j['results']:
+		for item in output['results']:
 			lat = item['geometry']['location']['lat']
 			lng = item['geometry']['location']['lng']
-			# loc = item['geometry']['location']
+			
 	else: 
-		print j['status']
+		print output['status']
 	return lat, lng
 
-l = getlocation()
-lat = l[0]
-lng = l[1]
-payload2 = { 
-		'radius': 4000, 
-		'type': 'electronics_store', 
-		'keyword': 'sieu+thi+dien+may', 
-		'key': key
-}
+def geturl2():
 
-url2 = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%f,%f' %(lat, lng))
+	loc = getlocation()
+	lat = loc[0]
+	lng = loc[1]
+	payload2 = { 
+			'radius': 4000, 
+			'type': type, 
+			'keyword': keyword, 
+			'key': key
+	}
 
-def getaddress():
+	url2 = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%f,%f' %(lat, lng))
+	return url2, payload2
+
+def result():
 	"This function returns nearest addresses of a given location"
-	r2 = requests.get(url2, params=payload2)
-	j2 = json.loads(r2.text)
+	u = geturl2()
+	r = requests.get(u[0], params=u[1])
+	output = json.loads(r.text)
 	
-	if j2['status'] == 'OK':
-		for item in j2['results']:
-			name = item['name']
-			address = item['vicinity']
+	if output['status'] == 'OK':
+		for item in output['results']:
+			name = (item['name'])
+			address = (item['vicinity'])
 			print ('Store: {} \t Address: {}\n').format(name, address)
 	else:
-		print j2['status']
+		print output['status']
 	
-	# return name, address
-	
-print getaddress()
+print result()
